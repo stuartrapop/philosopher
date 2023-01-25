ba2366bc8a10bb580d0d5113fb1641703accee8d
Stuart Rapoport <stuartrapop@gmail.com>
2023-01-25 20:46:20 +0100

    still working on thread race conditions

--- philo/main/philosopher_3.c
+++ philo/main/philosopher_3.c
@@ -6,7 +6,7 @@
 /*   By: srapopor <srapopor@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/25 16:07:29 by srapopor          #+#    #+#             */
-/*   Updated: 2023/01/25 19:13:51 by srapopor         ###   ########.fr       */
+/*   Updated: 2023/01/25 20:45:18 by srapopor         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ int	check_eating_time(t_ph *phs, int ph_number, int *pass_cycle_test)
 	{
 		pthread_mutex_lock(&phs[0].game->monitor);
 		phs[ph_number].game->is_over = 1;
-		pri_mut(&(phs[0].game->print), "%d %d has died\n", ts, ph_number);
+		pri_mut(&(phs[0].game->print), "%d %d has died\n", ts, ph_number + 1);
 		pthread_mutex_unlock(&phs[0].game->monitor);
 		return (1);
 	}
